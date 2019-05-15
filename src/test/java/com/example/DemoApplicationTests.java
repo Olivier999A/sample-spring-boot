394461f5e57238055f2f8b1667f26929cd2760eb
olivier <a@b.c>
2019-05-15 17:00:45 +0200

    version

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+	@Ignore
 	@Test
 	public void contextLoads() {
 	}
