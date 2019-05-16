45eb78f29eff5439c24c1a4172b16d0bfcaf455b
olivier <a@b.c>
2019-05-16 10:15:13 +0200

    test1

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,5 +12,9 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
+	@Test	
+	public void shouldAnswerWithTrue()
+   	{
+        assertTrue( true );
+    	}
 }
